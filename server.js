a342f78cc2a878262c28d35d3a5475c05cd89862
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-08-01 09:41:43 +0200

    s

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const userRoutes = require("./routes/user.js");
 const todo = require("./routes/todo.js");
 const cookieParser = require("cookie-parser");
-
 mongoose
   .connect("mongodb://localhost/todo")
   .then(() => {
@@ -16,8 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/user", userRoutes);
-app.use("/todo", todo);
+app.use("/.netlify/functions/api/user", userRoutes);
+app.use("/.netlify/functions/api/todo", todo);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
