7759d89332aceb8e13166ab2f81a428533d11e3b
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-08-03 17:21:45 +0200

    test

--- functions/src/routes/user.js
+++ functions/src/routes/user.js
@@ -1,10 +1,6 @@
-const express = require("express");
-const router = express.Router();
 const bcrypt = require("bcrypt");
 const Users = require("../model/model.js");
 const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
-require("dotenv").config({ path: "../../../.env" });
 
 const emailRegex =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -12,51 +8,46 @@ const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
 //Using secret_key here for testing purpose
 const SECRET_KEY = "Alban";
 
-const register = async (req, res) => {
-  const { full_name, email, password, confirmPassword } = req.body;
-  const result = await Users.findOne({ email: email }).catch((err) =>
-    res.send(err)
-  );
-
-  if (result) {
-    return res.status(409).send({ message: "Already exists" });
-  }
-
-  if (password !== confirmPassword) {
-    return res.status(401).send({ message: "Password don`t match" });
-  }
-
-  if (emailRegex.test(email) && passwordRegex.test(password)) {
-    req.body.password = await hashPassword(password);
-
-    const userCreate = await Users.create(req.body).catch((err) => {
-      return res.send(err);
-    });
-
-    res.status(200).send({ message: "Registred Succesfully" });
-  } else {
-    return res
-      .status(400)
-      .send({ message: "Email and/or password is invalid" });
+const hashPassword = async (password) => {
+  try {
+    const hash = await bcrypt.hash(password, 10);
+    return hash;
+  } catch (err) {
+    console.error("Error while hashing password:", err);
+    throw err;
   }
 };
 
-const login = async (req, res) => {
-  const { email, password } = req.body;
+const verifyPassword = async (userPassword, storedHash) => {
+  try {
+    const isPasswordMatch = await bcrypt.compare(userPassword, storedHash);
+    return isPasswordMatch;
+  } catch (err) {
+    console.error("Error while verifying password:", err);
+    throw err;
+  }
+};
+
+const login = async (event, context) => {
+  const { email, password } = JSON.parse(event.body);
   const user = await Users.findOne({ email: email }).catch((err) =>
     res.send(err)
   );
 
   if (!user) {
-    return res
-      .status(409)
-      .send({ message: "Email and/or password is invalid" });
+    return {
+      statusCode: 409,
+      body: JSON.stringify({ message: "Email and/or password is invalid" }),
+    };
   }
 
   const checkPassword = await verifyPassword(password, user.password);
 
   if (!checkPassword) {
-    res.status(400).send({ message: "Email and/or password is invalid" });
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Email and/or password is invalid" }),
+    };
   }
 
   const payload = {
@@ -65,45 +56,62 @@ const login = async (req, res) => {
   };
   const token = jwt.sign(payload, SECRET_KEY);
 
-  res.cookie("jwt", token, { httpOnly: true });
-  res.status(200).send({ message: "Loggin Sucessfully", jwt: token });
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Loggin Successfully", jwt: token }),
+    headers: {
+      "Content-Type": "application/json",
+      "Set-Cookie": `jwt=${token}; HttpOnly; Path=/`,
+    },
+  };
 };
 
-async function hashPassword(password) {
-  try {
-    const hash = await bcrypt.hash(password, 10);
-    return hash;
-  } catch (err) {
-    console.error("Error while hashing password:", err);
-    throw err;
-  }
-}
+const register = async (event, context) => {
+  const { full_name, email, password, confirmPassword } = JSON.parse(
+    event.body
+  );
+  const result = await Users.findOne({ email: email }).catch((err) =>
+    res.send(err)
+  );
 
-async function verifyPassword(userPassword, storedHash) {
-  try {
-    const isPasswordMatch = await bcrypt.compare(userPassword, storedHash);
-    return isPasswordMatch;
-  } catch (err) {
-    console.error("Error while verifying password:", err);
-    throw err;
-  }
-}
-
-function authenticateToken(req, res, next) {
-  const token = req.header("Authorization")?.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ message: "Access denied. Token missing." });
+  if (result) {
+    return {
+      statusCode: 409,
+      body: JSON.stringify({ message: "Already exists" }),
+    };
   }
 
-  jwt.verify(token, secretKey, (err, user) => {
-    if (err) {
-      return res.status(403).json({ message: "Invalid token." });
-    }
+  if (password !== confirmPassword) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ message: "Password don't match" }),
+    };
+  }
 
-    req.user = user;
-    next();
-  });
-}
+  if (emailRegex.test(email) && passwordRegex.test(password)) {
+    const hashedPassword = await hashPassword(password);
 
-module.exports = { login, register };
+    const newUser = new Users({
+      full_name,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Registered Successfully" }),
+    };
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Email and/or password is invalid" }),
+    };
+  }
+};
+
+module.exports = {
+  login,
+  register,
+};
