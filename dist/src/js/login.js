541af9e1e95012c13c280f88fe0a88ebdd86e353
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-08-03 13:54:48 +0200

    /

--- dist/src/js/login.js
+++ dist/src/js/login.js
@@ -12,7 +12,7 @@ form.addEventListener("submit", (e) => {
 
 async function fetchData(loginData) {
   try {
-    const response = await fetch("/.netlify/functions/user/login", {
+    const response = await fetch("/.netlify/functions/user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
