a167d339005bdeafcb4f4c2629b895eb204bdcf0
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-08-03 18:21:31 +0200

    test

--- functions/login.js
+++ functions/login.js
@@ -12,35 +12,35 @@ const SECRET_KEY = "Alban";
 exports.handler = async (event, context) => {
   const body = JSON.parse(event.body);
   const { email, password } = body;
-  const user = await Users.findOne({ email: email }).catch((err) =>
-    res.send(err)
-  );
+  // const user = await Users.findOne({ email: email }).catch((err) =>
+  //   res.send(err)
+  // );
 
-  if (!user) {
-    return {
-      statusCode: 409,
-      body: JSON.stringify({ message: "Email and/or password is invalid" }),
-    };
-  }
+  // if (!user) {
+  //   return {
+  //     statusCode: 409,
+  //     body: JSON.stringify({ message: "Email and/or password is invalid" }),
+  //   };
+  // }
 
-  const checkPassword = await verifyPassword(password, user.password);
+  // const checkPassword = await verifyPassword(password, user.password);
 
-  if (!checkPassword) {
-    return {
-      statusCode: 409,
-      body: JSON.stringify({ message: "Email and/or password is invalid" }),
-    };
-  }
+  // if (!checkPassword) {
+  //   return {
+  //     statusCode: 409,
+  //     body: JSON.stringify({ message: "Email and/or password is invalid" }),
+  //   };
+  // }
 
-  const payload = {
-    user_id: user._id,
-    email: email,
-  };
-  const token = jwt.sign(payload, SECRET_KEY);
+  // const payload = {
+  //   user_id: user._id,
+  //   email: email,
+  // };
+  // const token = jwt.sign(payload, SECRET_KEY);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Logged in succefully", jwt: jwt }),
+    body: JSON.stringify({ message: email, jwt: jwt }),
     headers: {
       "Set-Cookie": `jwt=${token}; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=3600`, // Set the cookie with HttpOnly and Secure flags
     },
