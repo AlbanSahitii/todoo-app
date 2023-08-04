ce346c878c4a87ab1c901017f7364472cb4a6057
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-08-04 17:51:01 +0200

    debuuggg

--- functions/login.js
+++ functions/login.js
@@ -17,6 +17,7 @@ exports.handler = async (event, context) => {
       body: JSON.stringify({ message: err }),
     };
   });
+  console.log("user. ", user);
   console.log("user.password ", user.password);
   console.log("password ", password);
 
