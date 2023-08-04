afd2448751f5de84983d00ad5be8cc6ef418fa7d
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-08-04 21:25:07 +0200

    debugging

--- functions/login.js
+++ functions/login.js
@@ -15,7 +15,9 @@ exports.handler = async (event, context) => {
   console.log(`Starting to connect to db`);
 
   try {
-    const user = await Users.find({ email: email });
+    const user = await Users.find({ email: email }).then((data) =>
+      console.log(data)
+    );
   } catch (error) {
     console.log(`getting user error ${error}`);
   }
