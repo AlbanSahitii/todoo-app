11cf25fa7ccc3168866de9a512935acbabd1d4f2
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-08-04 21:46:21 +0200

    debugging

--- functions/login.js
+++ functions/login.js
@@ -14,47 +14,49 @@ exports.handler = async (event, context) => {
 
   console.log(`Starting to connect to db`);
 
-  const user = await Users.find({ email: email })
-    .then((data) => console.log(data))
+  user = await Users.find({ email: email })
+    .then(async (data) => {
+      console.log("Data : ", data);
+
+      console.log("user. ", user);
+      console.log("user.password ", user.password);
+      console.log("password ", password);
+
+      console.log(`done got user`);
+      if (!user) {
+        return {
+          statusCode: 409,
+          body: JSON.stringify({ message: "Email and/or password is invalid" }),
+        };
+      }
+
+      console.log(`checking pww`);
+      const checkPassword = await verifyPassword(password, user.password);
+
+      if (!checkPassword) {
+        return {
+          statusCode: 409,
+          body: JSON.stringify({ message: "Email and/or password is invalid" }),
+        };
+      }
+
+      console.log(`sending payload`);
+      const payload = {
+        user_id: user._id,
+        email: email,
+      };
+      // const token = jwt.sign(payload, SECRET_KEY);
+
+      console.log(`returnin good`);
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Logged in succefully", jwt: jwt }),
+        // headers: {
+        //   "Set-Cookie": `jwt=${token}; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=3600`, // Set the cookie with HttpOnly and Secure flags
+        // },
+      };
+    })
     .catch((error) => console.log(`getting user error ${error}`));
-
-  console.log("user. ", user);
-  console.log("user.password ", user.password);
-  console.log("password ", password);
-
-  console.log(`done got user`);
-  if (!user) {
-    return {
-      statusCode: 409,
-      body: JSON.stringify({ message: "Email and/or password is invalid" }),
-    };
-  }
-
-  console.log(`checking pww`);
-  const checkPassword = await verifyPassword(password, user.password);
-
-  if (!checkPassword) {
-    return {
-      statusCode: 409,
-      body: JSON.stringify({ message: "Email and/or password is invalid" }),
-    };
-  }
-
-  console.log(`sending payload`);
-  const payload = {
-    user_id: user._id,
-    email: email,
-  };
-  // const token = jwt.sign(payload, SECRET_KEY);
-
-  console.log(`returnin good`);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Logged in succefully", jwt: jwt }),
-    // headers: {
-    //   "Set-Cookie": `jwt=${token}; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=3600`, // Set the cookie with HttpOnly and Secure flags
-    // },
-  };
 };
 
 async function verifyPassword(userPassword, storedHash) {
