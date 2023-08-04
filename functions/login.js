0f6b5dcd4dcf4b551afdf3c22b5ccb14c08efc30
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-08-04 22:32:31 +0200

    error handler

--- functions/login.js
+++ functions/login.js
@@ -64,20 +64,5 @@ exports.handler = async (event, context) => {
         body: JSON.stringify("succefully"),
       };
     })
-    .catch((error) => {
-      return {
-        statusCode: 404,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers":
-            "Origin, X-Requested-With, Content-Type, Accept",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Methods": "*",
-          "Access-Control-Max-Age": "2592000",
-          "Access-Control-Allow-Credentials": "true",
-          //   "Set-Cookie": `jwt=${token}; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=3600`, // Set the cookie with HttpOnly and Secure flags
-        },
-        body: JSON.stringify("erroor", error),
-      };
-    });
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
