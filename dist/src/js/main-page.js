39419b22f49851c13b9543ed22ce7351fb127c84
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-08-08 14:57:13 +0200

    added api call in frontend

---
+++ dist/src/js/main-page.js
@@ -0,0 +1,23 @@
+const token = localStorage.getItem("token");
+const _id = localStorage.getItem("id");
+async function fetchData() {
+  await fetch("/.netlify/functions/getTodos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(_id),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      if (err) {
+        throw new Error("Request failed:", err);
+      }
+    });
+}
+
+fetchData();
